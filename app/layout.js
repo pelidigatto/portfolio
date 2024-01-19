1933abf583510b12267d03e0aa92b89f3e96b0f3
pelidigatto <florianthoenelt@outlook.de>
2024-01-19 13:27:27 +0100

    CookieConsent hinzugefügt

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,7 @@ import Providers from './providers'
 import Navbar from './components/navbar'
 import Footer from './components/footer'
 import { useEffect } from 'react';
-
-
+import CookieConsent from "react-cookie-consent";
 
 
  function RootLayout({ children }) {
@@ -23,9 +22,18 @@ import { useEffect } from 'react';
             <div className='container w-100'>
               {children}
               <Footer />  
-            </div>
-            
+            </div>      
           </Providers>
+          <CookieConsent
+             location="center"
+             buttonText="Akzeptieren"
+             cookieName="cookieConsent"
+             style={{ background: "var(--background-color)",color:"var(--text-color)", borderTop: "1px solid var(--text-color)" }}
+             buttonStyle={{ color: "var(--text-color)", backgroundColor:"#4bb74a" ,fontSize: "18px" }}
+             expires={150}
+             overlay={true}
+             >Diese Webseite verwendet Cookies und verarbeitet Daten, um die Benutzererfahrung zu verbessern. Durch die Nutzung unserer Webseite stimmen Sie der Verwendung aller Cookies gemäß unserer Cookie-Richtlinie zu.</CookieConsent>
+
       </body>
     </html>
   )
