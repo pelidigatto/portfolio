ad19c17ff2ce137d34035bf54fe6c082a6c708d8
pelidigatto <florianthoenelt@outlook.de>
2024-02-02 15:06:11 +0100

    responsive skillbar

--- app/components/footer.js
+++ app/components/footer.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 function footer(){
     var year = new Date().getFullYear();
     return(    
-        <div className="row border-top text-center p-2">
+        <div className="row border-top text-center p-2 m-0">
             <div className="col-6">© {year} Florian Thönelt</div>
             <div className="col-6"><Link href="/impressum/">Impressum</Link> / <Link href="/datenschutz/">Datenschutz</Link></div>          
         </div>
