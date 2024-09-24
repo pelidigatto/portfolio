6f0cba9b258b227f7c949cb325d9040d4215b4c9
pelidigatto <florianthoenelt@outlook.de>
2024-09-24 06:37:08 +0200

    matomo

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ import Navbar from './components/navbar'
 import Footer from './components/footer'
 import { useEffect } from 'react';
 
+import Script from 'next/script';
 
  
 
@@ -36,9 +37,35 @@ config.autoAddCss = false; /* eslint-disable import/first */
         <title>Florian Thönelt</title>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta name="google-site-verification" content="PHm4IewC9ZmRYNOZMFg3JRODRRivkdmW3mBwiN4a7FM" />
-        <script type="text/javascript" src="//cdn.cookie-script.com/s/102ae36c00073d80e7b5b6dd4575b25b.js"></script>
+        {/* <script type="text/javascript" src="//cdn.cookie-script.com/s/102ae36c00073d80e7b5b6dd4575b25b.js"></script> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta name="description" content="Florian Thönelt - Webentwickler aus Hannover" />
+        <meta name="keywords" content="Webentwickler, Hannover, SEO, PHP, JavaScript, HTML, CSS, Laravel, React, Node.js" />
+        <meta name="author" content="Florian Thönelt" />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="google" content="nositelinkssearchbox" />
+        <meta name="google" content="notranslate" />
+             
 
 
+
+
+        <Script id="matomo-script" strategy="afterInteractive">
+            {`
+              var _paq = window._paq = window._paq || [];
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u = '//matomo.thnlt.de/';
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '2']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
+            `}
+        </Script>
       </head>
       <body>
           <Providers>
@@ -48,16 +75,7 @@ config.autoAddCss = false; /* eslint-disable import/first */
               <Footer />  
             </div>      
           </Providers>
-          {/* <CookieConsent
-             location="center"
-             buttonText="Akzeptieren"
-             cookieName="cookieConsent"
-             style={{ background: "var(--background-color)",color:"var(--text-color)", borderTop: "1px solid var(--text-color)" }}
-             buttonStyle={{ color: "var(--text-color)", backgroundColor:"#4bb74a" ,fontSize: "18px" }}
-             expires={150}
-             overlay={true}
-             >Diese Webseite verwendet Cookies und verarbeitet Daten, um die Benutzererfahrung zu verbessern. Durch die Nutzung unserer Webseite stimmen Sie der Verwendung aller Cookies gemäß unserer Cookie-Richtlinie zu.</CookieConsent> */}
-
+        
       </body>
     </html>
   )
