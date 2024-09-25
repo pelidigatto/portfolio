27a5ad84940fcd6568c5ed1350c6fab8ea96438c
pelidigatto <florianthoenelt@outlook.de>
2024-09-25 05:28:00 +0200

    New Logo and Favicon

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,15 +1,27 @@
 import ThemeSwitcher from "./ThemeSwitcher"
 import Link from "next/link"
+import Image from "next/image"
 export default function Navbar(){
     return (
         <nav className="navbar fixed-top navbar-expand-lg p-3 border-bottom">
-            <div className="container-fluid">
-                <Link href="/" className="navbar-brand">thoenelt.dev</Link>
+            <div className="container-fluid d-flex justify-content-center align-items-center">
+                <Image
+                        src={'/header_logo.png'}
+                        width={25}
+                        height={25}
+                        alt={"Screenshot von "}  
+                        className="img-fluid me-2"  
+                />
+                <Link href="/" className="navbar-brand">
+
+                    thoenelt.dev
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarText">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        
                         <li className="nav-item">
                             <Link href="/" className="nav-link">Home</Link>
                         </li>
